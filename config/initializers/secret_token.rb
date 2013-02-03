9d63ccd68b368b30abcaa6216a5dd5082d2f0343
Rapi Castillo <rapi.castillo@gmail.com>
2013-02-03 15:42:29 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Resignako::Application.config.secret_token = '6b8ee582752bf5202937aa41d84dffeea5541ade1d911b5a1267ee4e0ae6e23d205859d15eb4cd47c4206aace0d54617295e9e515e815fbfbd0207700404d976'
