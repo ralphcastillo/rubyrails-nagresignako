1d1376f6d3b5257166221c8b2daaa6c0e65ef7a7
Rapi Castillo <rapi.castillo@gmail.com>
2013-02-07 16:58:45 +0800

    Changed layout to conform to study

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -4,4 +4,12 @@ NAGRESIGNAKO.common = new Object;
 
 NAGRESIGNAKO.common.init = function (){
   // Your js code for all pages here
+  
+  $(document).ajaxComplete(function(){
+    try{ FB.XFBML.parse(); }catch(ex){}
+    
+    try { twttr.widgets.load(); } catch(ex) {}
+    
+    try { gapi.plusone.go(); } catch (ex) {}
+  });
 }
