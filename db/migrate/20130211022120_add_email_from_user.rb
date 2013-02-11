428610ff274a2c7f99cfd8084f33b07f2ca0fc45
Rapi Castillo <rapi.castillo@gmail.com>
2013-02-11 18:05:10 +0800

    Minor chcane

--- db/migrate/20130211022120_add_email_from_user.rb
+++ db/migrate/20130211022120_add_email_from_user.rb
@@ -1,5 +1,5 @@
 class AddEmailFromUser < ActiveRecord::Migration
   def change
-    add_column :users, :email, :string #, :null => false
+    add_column :users, :email, :string, :null => false
   end
 end
