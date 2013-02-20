4bdee5479e32318d6e0b4d85978e4eab569bcfe2
iche <iche@Iche-Velascos-MacBook-Pro.local>
2013-02-20 19:55:47 +0800

    Fixed host name issue in Prod

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,7 +33,7 @@ class PostsController < ApplicationController
       
         if @post.save
           session[:post_id] = @post.id
-          redirect_to client.authorization.authorize_url(:redirect_uri => "http://#{ENV['URL']}/posts/callback/" ,
+          redirect_to client.authorization.authorize_url(:redirect_uri => "http://#{request.host_with_port}/posts/callback/" ,
       :client_id => '488599381199316',:scope => 'email')
         else
           respond_to do |format|
@@ -59,7 +59,7 @@ class PostsController < ApplicationController
       
       respond_to do |format|
         if @post.save
-          UserMailer.verify_user(@user, @post, "http://#{ENV['URL']}/posts/verify").deliver        
+          UserMailer.verify_user(@user, @post, "http://#{request.host_with_port}/posts/verify").deliver        
           flash[:notice] = 'Please check your mail to verify your post. Thank you for posting.'
           format.html  { redirect_to :action => "new" } 
         else
@@ -178,12 +178,12 @@ class PostsController < ApplicationController
   end
   
   def fb_verify    
-    redirect_to client.authorization.authorize_url(:redirect_uri => "http://#{ENV['URL']}/posts/callback/" ,
+    redirect_to client.authorization.authorize_url(:redirect_uri => "http://#{request.host_with_port}/posts/callback/" ,
       :client_id => '488599381199316',:scope => 'email')
   end
   
   def callback
-    @access_token = client.authorization.process_callback(params[:code], :redirect_uri => "http://#{ENV['URL']}/posts/callback/")
+    @access_token = client.authorization.process_callback(params[:code], :redirect_uri => "http://#{request.host_with_port}/posts/callback/")
     session[:access_token] = @access_token
     @fb_user = client.selection.me.info!
    
