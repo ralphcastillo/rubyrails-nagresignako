2f9bcef36914b8111787125894c6c66d7332708c
Rapi Castillo <rapi.castillo@gmail.com>
2013-03-07 10:16:59 +0800

    Minor fixes

--- app/assets/javascripts/posts/single.js
+++ app/assets/javascripts/posts/single.js
@@ -1,13 +1,3 @@
 // posts/single.js
 
-NAGRESIGNAKO.posts.single = function() {
-	fyre.conv.load({}, [conversationInfo], function(widget) {
-    widget.on('commentPosted', function (data) {
-        // Do something, perhaps using data
-        	LF.CommentCount({ replacer: function(element, count) { 
-    			element.innerHTML = count +' Comment'+ (count === 1 ? '' : 's');
-				}
-			});
-	    });
-	});
-}
+NAGRESIGNAKO.posts.single = function() {}
